b2cc55e3ab84f78afa64cd51aa9a00e954ca857e
greencoffee1635 <166354@naver.com>
2022-01-20 15:15:54 +0900

    [#1]feat: create react app

---
+++ src/shared/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    :root{
+    --main: #D3455B;
+    }
+    /* @import url('https://fonts.googleapis.com/css2?family=Cedarville+Cursive&display=swap'); */
+  * {
+        /* font-family: 'Cedarville Cursive', cursive; */
+        /* letter-spacing: -0.015em; */
+    }
+`;
+
+export default GlobalStyle;
---
+++ src/shared/textStyle.js
@@ -0,0 +1,36 @@
+import { css } from 'styled-components';
+
+// 아래는 예시입니다.
+
+// 로고
+export const logo = css`
+  font-size: 1.8rem;
+  font-weight: 600;
+`;
+
+// Header Menu
+export const head_1 = css`
+  font-size: 1.1rem;
+  font-weight: 500;
+`;
+
+
+// // 문항 제목
+// export const sub_1 = css`
+//   font-size: 1.4rem;
+//   font-weight: 400;
+//   line-height: 3rem;
+//   letter-spacing: -0.05rem;
+// `;
+
+// // 문항 답변
+// export const sub_2 = css`
+//   font-size: 1.4rem;
+//   font-weight: 400;
+// `;
+
+// // 버튼
+// export const button = css`
+//   font-size: 1rem;
+//   font-weight: 600;
+// `;
