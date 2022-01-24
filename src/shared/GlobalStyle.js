95380a0fe107c5838bd6ca79aec71368aca57141
greencoffee1635 <166354@naver.com>
2022-01-24 20:20:33 +0900

    [#6] style: 메인 컬러 및 폰트 지정, 전역 설정

--- src/shared/GlobalStyle.js
+++ src/shared/GlobalStyle.js
@@ -2,12 +2,12 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     :root{
-        --main: blue;
+        --main: #279eea;
     }
     
-    /* @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap'); */
+    @import url('https://fonts.googleapis.com/css2?family=Jua&display=swap');
   * {
-        /* font-family: 'Montserrat'; */
+        font-family: 'Jua', sans-serif;
         box-sizing: border-box;
         /* font-family: 'Cedarville Cursive', cursive;
         letter-spacing: -0.015em; */
