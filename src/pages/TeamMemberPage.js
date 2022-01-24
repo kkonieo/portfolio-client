7f97096a5c3199efde5c0cfabd7b9d31d355f4c2
leaisrevolution <leaisrevolution@gmail.com>
2022-01-24 19:42:59 +0900

    "[#4] edit : 세팅시작 시작 "

--- src/pages/TeamMemberPage.js
+++ src/pages/TeamMemberPage.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const TeamMemberPage = () => {
-  return <div></div>;
+  return (
+    <MemberCard>
+      <p>Front-end, LEA</p>
+    </MemberCard>
+  );
 };
+
+const MemberCard = styled.div`
+  width: 350px;
+  height: 350px;
+
+  background-color: var(--main);
+  border-radius: 50%;
+
+  text-align: center;
+  font-size: 30px;
+  line-height: 200px;
+
+  color: #ffffff;
+`;
 
 export default TeamMemberPage;
