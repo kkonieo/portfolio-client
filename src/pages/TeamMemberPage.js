4ba1de8010b547525c49b6937bd8879f6a2e6483
leaisrevolution <leaisrevolution@gmail.com>
2022-01-25 17:35:56 +0900

    [#4] edit : 팀 이미지 추가

--- src/pages/TeamMemberPage.js
+++ src/pages/TeamMemberPage.js
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 
 import MembersData from '../Data/MembersData';
 
+
+const env = process.env;
+env.PUBLIC_URL = env.PUBLIC_URL || "";
+
+
 const TeamMemberPage = (props) => {
 
   let [card, setCard] = useState(MembersData);
@@ -10,6 +15,7 @@ const TeamMemberPage = (props) => {
   function MemberCards(props) {
     return (
       <MemberCard>
+        <img alt="" src={process.env.PUBLIC_URL + `/Image/image${props.card.id}.png`} width="370px" />
         <p>{props.card.position} <br /> <b>{props.card.name}</b></p>
       </MemberCard>
     )
@@ -29,24 +35,52 @@ const TeamMemberPage = (props) => {
 
 const TeamSection = styled.div`
   margin-top: 80px;
-  // display: flex;
-  // flex-direction: row;
+  width: 100%;
+  height: 100vh;
+
+  // background-color: var(--main);
+
 `;
 
 const MemberCard = styled.div`
-  width: 350px;
-  height: 350px;
+
+  justify-content: center;
+  width: 320px;
+  height: 320px;
+  cursor: pointer;
+  overflow: hidden;
+
+  float: left;
 
   background-color: var(--main);
   border-radius: 50%;
+  dispaly: flex;
+  margin-left: 60px;
+  margin-bottom: 30px;
 
-  text-align: center;
+  // text-align: center;
   font-size: 30px;
   // line-height: 200px;
-  padding: 130px 0;
+  // padding: 130px 0;
 
   color: #ffffff;
   align-items: center;
+  position: relative;
+
+  img {
+    object-fit: cover;
+    background-size: contain;
+
+    position: absolute;
+
+    top: 0px;
+    left: 0px;
+
+    width: 100%;
+    height: 100%;
+  }
+
+
 `;
 
 export default TeamMemberPage;
