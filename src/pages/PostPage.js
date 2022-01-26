a8caa4ba13ce5c7e158e9d54d52ffc0e96790b95
greencoffee1635 <166354@naver.com>
2022-01-27 00:36:23 +0900

    [#14] style: 포스트 페이지 기초 레이아웃

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const PostPage = () => {
-  return <div>Post</div>;
+// components & elements
+import Header from '../components/Header';
+import { PostTemplate } from '../components/Template';
+import { head_2 } from '../shared/textStyle';
+
+const PostPage = (props) => {
+  return (
+    <PostTemplate>
+      <Header page="headermenu" />
+
+      <TitleWrapper>
+        <Title>Title</Title>
+        <div></div>
+      </TitleWrapper>
+
+      <TitleWrapper>
+        <Title>Keyword</Title>
+        <div></div>
+      </TitleWrapper>
+
+      <TitleWrapper>
+        <Title>Contents</Title>
+        <div></div>
+      </TitleWrapper>
+
+      <TitleWrapper>
+        <Title>Comments</Title>
+        <div></div>
+      </TitleWrapper>
+    </PostTemplate>
+  );
 };
+
+const TitleWrapper = styled.section`
+  ${head_2};
+`;
+
+const Title = styled.h2`
+  ${head_2};
+  color: var(--main);
+`;
 
 export default PostPage;
