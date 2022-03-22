b4ade9b2b590e40d17b987d0352e76568ba69c52
greencoffee1635 <166354@naver.com>
2022-03-23 02:56:26 +0900

    [#16] edit: 오타 수정

--- src/utils/ScrollToTop.js
+++ src/utils/ScrollToTop.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
-export default function ScrollToTop() {
+const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
@@ -10,4 +9,6 @@ export default function ScrollToTop() {
   }, [pathname]);
 
   return null;
-}
+};
+
+export default ScrollToTop;
